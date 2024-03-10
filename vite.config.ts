3d5fa63a7832b9d6c9883bf45874b06ac1d4ea74
vashkevichma <vashkevichma@pik.ru>
2024-03-10 18:30:36 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': '/src.*',
+      '@components': '/src/components',
+      '@store': '/src/store',
+      '@atoms': '/src/components/atoms',
+      '@molecules': '/src/components/molecules',
+      '@organisms': '/src/components/organisms',
+      '@pages': '/src/components/pages',
+    },
+  },
+});
