4df372438ca03ed5afb4a6494822fa95c277ff9c
vashkevichma <maksim1998vashkevich@gmail.com>
2024-04-01 21:37:27 +0300

    [fix] universal paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@src': '/src',
-      '@components': '/src/components',
-      '@store': '/src/store',
-      '@atoms': '/src/components/atoms',
-      '@molecules': '/src/components/molecules',
-      '@organisms': '/src/components/organisms',
-      '@pages': '/src/components/pages',
+      '@src': resolve(__dirname, './src'),
+      '@store': resolve(__dirname, './src/store'),
+      '@atoms': resolve(__dirname, './src/components/atoms'),
+      '@pages': resolve(__dirname, './src/components/pages'),
+      '@components': resolve(__dirname, './src/components'),
+      '@molecules': resolve(__dirname, './src/components/molecules'),
+      '@organisms': resolve(__dirname, './src/components/organisms'),
     },
   },
 });
